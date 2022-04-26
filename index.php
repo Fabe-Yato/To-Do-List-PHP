900f56d0bbad40417b64aa7e5dfbc1dc26b0159a
Fabiano <Fabibinho2@gmail.com>
2022-04-26 11:33:46 -0300

    Adicionando os arquivos do projeto

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <div>
+                 <img src="./imgs/list.png" width="35px">
+                 <a href="#" class="navbar-brand text-white display-1">Super ToDo</a>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container app">
+        <div class="row">
+            <div class="col-md-3 menu">
+                <ul>
+                   <li class="list-group-item active">
+                      <a href="#">Tarefas Pendentes</a> 
+                    </li>
+                   <li class="list-group-item">
+                      <a href="nova_tarefa.php">Nova Tarefa</a> 
+                    </li>
+                   <li class="list-group-item">
+                      <a href="todas_tarefas.php">Todas as Tarefas</a> 
+                    </li> 
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <?php 
+    
+    
+    ?>
+</body>
+</html>
